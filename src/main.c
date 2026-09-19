efb5e2db048bc59f0b1e40857efa334670847f36
Shyane Novelle <shyanenovelle.canayan@g.msuiit.edu.ph>
2026-09-20 00:31:17 +0800

    Initialize ESP32 PlatformIO project

--- src/main.c
+++ src/main.c
@@ -2,7 +2,13 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-void app_main(void) {
+void app_main(void)
+{
     printf("BCA152 FreeRTOS Multisensor\n");
     printf("System starting...\n");
+    fflush(stdout);
+
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
