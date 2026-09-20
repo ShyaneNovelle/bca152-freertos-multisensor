f613c2578d22ea932970ee7ffc7b652aec0f91a0
Shyane Novelle <shyanenovelle.canayan@g.msuiit.edu.ph>
2026-09-20 09:55:20 +0800

    Configure initial Wokwi simulation

--- src/main.c
+++ src/main.c
@@ -4,11 +4,18 @@
 
 void app_main(void)
 {
+    
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    printf("\n\n==============================\n");
     printf("BCA152 FreeRTOS Multisensor\n");
     printf("System starting...\n");
+    printf("==============================\n\n");
     fflush(stdout);
 
     while (1) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("BCA152 FreeRTOS Multisensor running...\n");
+        fflush(stdout);
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
